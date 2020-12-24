ee679dd88d424799e631049b66b0aaee9ba9e623
MbuguaCaleb <mbuguacaleb30@gmail.com>
2020-12-24 10:20:06 +0300

    naviagation

--- recipe-shopping-list-app/src/app/app.component.ts
+++ recipe-shopping-list-app/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'recipe-shopping-list-app';
+  loadedFeature = 'recipe';
+  
+  onNavigate(feature:string){
+    this.loadedFeature = feature;
+  }
+
 }
